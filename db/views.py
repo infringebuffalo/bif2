d06ac9af1129ba2e9ebe674b30f1ad38bcc3eb5d
davepape <depape@buffalo.edu>
2018-01-26 10:20:05 -0800

    add some logging

--- db/views.py
+++ db/views.py
@@ -42,6 +42,7 @@ def confirmProposal(request,id):
     prop = get_object_or_404(Proposal, pk=id)
     prop.status = Proposal.ACCEPTED     # Note that this will also undelete a deleted proposal - will leave it that way for now
     prop.save()
+    logInfo("confirmed proposal %d" % id, request)
     return render(request,'db/proposal_confirm.html', {'title':prop.title})
 
 
@@ -62,12 +63,15 @@ def createAccount(request):
     try:
         djuser = User.objects.create_user(email, email, password)
     except IntegrityError:
+        logError("Tried to create duplicate account '%s'"%email)
         err = "An account '%s' already exists" % email
         return render(request, 'db/new_account.html', { 'error': err })
     except:
+        logError("Unknown error creating account '%s'"%email)
         return render(request, 'db/new_account.html', { 'error': 'Failed to create account.' })
     bifuser = BIFUser.objects.create(user=djuser, phone='555-1212')
     login(request, authenticate(username=email,password=password))
+    logInfo("Created new account '%s'"%email)
     return redirect('index')
 
 
@@ -77,3 +81,27 @@ def proposal(request,id):
     infodict = json.loads(prop.info)
     context = {'prop':prop, 'prop_info':infodict}
     return render(request,'db/proposal.html', context)
+
+
+import logging
+
+def logInfo(message,request=None):
+    logger = logging.getLogger(__name__)
+    logger.info(logMessage(message,request))
+
+
+def logError(message,request=None):
+    logger = logging.getLogger(__name__)
+    logger.error(logMessage(message,request))
+
+
+def logMessage(message,request=None):
+    from datetime import datetime
+    username = 'anonymous-user'
+    if request and request.user and request.user.username and request.user.username != '':
+        username = request.user.username
+    ipaddr = 'ip-unknown'
+    if request:
+        ipaddr = request.META.get('REMOTE_ADDR')
+    return "%s %s %s: %s" % (datetime.now().strftime("%Y-%m-%d %X"),ipaddr,username, message)
+
