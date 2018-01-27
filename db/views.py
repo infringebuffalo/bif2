28b18fdf372e01ca053b69272c16ae2884ac940d
davepape <depape@buffalo.edu>
2018-01-26 21:32:04 -0800

    didn't use this - removing

--- db/views.py
+++ db/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login,authenticate
 from django.template import loader
 from django.core.mail import EmailMultiAlternatives
-from django.contrib.auth.decorators import permission_required
 
 from .models import *
 
