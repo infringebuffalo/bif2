a059787e0620cf4322ea168f4e07496b177b02b3
davepape <depape@buffalo.edu>
2018-07-13 14:45:48 -0400

    add calendar view

--- publicsite/templatetags/bif_schedule.py
+++ publicsite/templatetags/bif_schedule.py
@@ -16,11 +16,11 @@ def timeToString(t):
     m = t - h*100
     h = h % 24
     if h >= 12:
-        suffix = 'pm'
+        suffix = ' pm'
         if h > 12:
             h = h - 12
     else:
-        suffix = 'am'
+        suffix = ' am'
         if h == 0:
             h = 12
     if m != 0:
@@ -28,6 +28,15 @@ def timeToString(t):
     else:
         return '%d%s' % (h,suffix)
 
+def timeSpanToString(t0,t1):
+    start = timeToString(t0)
+    end = timeToString(t1)
+    if start[-2:] == "am" and end[-2:] == "am":
+        return start[:-3] + "-" + end
+    elif start[-2:] == "pm" and end[-2:] == "pm":
+        return start[:-3] + "-" + end
+    else:
+        return start + "-" + end
 
 @register.simple_tag
 def bif_listingRow(listing,proposal,venue,groupshow):
@@ -40,7 +49,7 @@ def bif_listingRow(listing,proposal,venue,groupshow):
     if listing.installation:
         retval += format_html('<td{}>installation</td>',tdflags)
     else:
-        retval += format_html('<td{}>{}-{}</td>',tdflags,timeToString(listing.starttime),timeToString(listing.endtime))
+        retval += format_html('<td{}>{}</td>',tdflags,timeSpanToString(listing.starttime,listing.endtime))
     if not proposal:
         retval += format_html('<td{}><a href="{}">{}</a></td>',tdflags,reverse('entityInfo',kwargs={'id':listing.who.id}),listing.who.title)
     if not venue:
@@ -54,6 +63,38 @@ def bif_listingRow(listing,proposal,venue,groupshow):
         for g in groupshows:
             if max(listing.starttime,g.starttime) <= min(listing.endtime,g.endtime):
                 groupshowlinks.append(format_html('({}<a href="{}">{}</a>)','part of ' if proposal else '',reverse('entityInfo',kwargs={'id':g.id}),g.title))
+        if len(groupshowlinks) > 0:
+            retval += format_html('<td class="groupshow">')
+            for g in groupshowlinks:
+                retval += g
+        else:
+            retval += format_html('<td></td>')
+
+    retval += format_html('</tr>\n')
+    return retval
+
+
+
+@register.simple_tag
+def bif_calendarRow(listing):
+    from django.urls import reverse
+    venuenote = ' (%s)'%listing.venuenote if listing.venuenote != '' else ''
+    tdflags = mark_safe(' class="cancelled"') if listing.cancelled else ""
+    retval = format_html('<tr>')
+
+    if listing.installation:
+        retval += format_html('<td{}>installation</td>',tdflags)
+    else:
+        retval += format_html('<td{}>{}</td>',tdflags,timeSpanToString(listing.starttime,listing.endtime))
+    retval += format_html('<td{}><a href="{}">{}</a></td>',tdflags,reverse('entityInfo',kwargs={'id':listing.who.id}),listing.who.title)
+    retval += format_html('<td{}><a href="{}">{}</a>{}</td>',tdflags,reverse('entityInfo',kwargs={'id':listing.where.id}),listing.where.name,venuenote)
+
+    groupshows = GroupShow.objects.filter(where=listing.where,date=listing.date)
+    groupshowlinks = []
+    if not listing.installation:
+        for g in groupshows:
+            if max(listing.starttime,g.starttime) <= min(listing.endtime,g.endtime):
+                groupshowlinks.append(format_html('(part of <a href="{}">{}</a>)',reverse('entityInfo',kwargs={'id':g.id}),g.title))
         if len(groupshowlinks) > 0:
             retval += format_html('<td class="groupshow">')
             for g in groupshowlinks:
