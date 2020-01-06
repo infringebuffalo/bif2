5fa4f9e549bc240bb320c764c7d5af39b59a2907
davepape <depape@buffalo.edu>
2020-01-06 11:25:35 -0800

    run this script to re-generate all the templates for proposal forms and editing proposal forms

---
+++ db/templates/db/makeforms.sh
@@ -0,0 +1,17 @@
+cp makeforminfo_start.py makeforminfo.py
+python danceform.py > dance_form.html
+python editdanceform.py > edit_dance_form.html
+python editfilmform.py > edit_film_form.html
+python editliteraryform.py > edit_literary_form.html
+python editmusicform.py > edit_music_form.html
+python edittheatreform.py > edit_theatre_form.html
+python editvenueform.py > edit_venue_form.html
+python editvisualartform.py > edit_visualart_form.html
+python editworkshopform.py > edit_workshop_form.html
+python filmform.py > film_form.html
+python literaryform.py > literary_form.html
+python musicform.py > music_form.html
+python theatreform.py > theatre_form.html
+python venueform.py > venue_form.html
+python visualartform.py > visualart_form.html
+python workshopform.py > workshop_form.html
