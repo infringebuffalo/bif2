9052d07d9fa8f16b2c7b22fc18c399e6cb7ca4b3
davepape <depape@buffalo.edu>
2020-01-06 11:24:58 -0800

    template file for when makeforms.sh is re-run

---
+++ db/templates/db/makeforminfo_start.py
@@ -0,0 +1,12 @@
+from .models import *
+import json
+
+def makeFormInfo_all():
+    makeFormInfo_music()
+    makeFormInfo_theatre()
+    makeFormInfo_visualart()
+    makeFormInfo_dance()
+    makeFormInfo_film()
+    makeFormInfo_literary()
+    makeFormInfo_workshop()
+
