34b23abbba7b38fa0a15670f03f6ac67267d9f11
davepape <depape@buffalo.edu>
2025-03-03 19:17:13 -0800

    fixed FormInfo usage when there are multiple festivals in db

--- db/views.py
+++ db/views.py
@@ -12,7 +12,7 @@ from .models import *
 
 import json
 
-submissionsClosed = 1
+submissionsClosed = 0
 
 def index(request):
     print(request)
@@ -66,9 +66,10 @@ def submit(request):
 
     defaultContact = None
     defaultBatch = None
+    fest = FestivalInfo.objects.last()
     if 'type' in infodict.keys():
         try:
-            formInfo = FormInfo.objects.get(showType=infodict['type'])
+            formInfo = FormInfo.objects.get(showType=infodict['type'],festival=fest)
             defaultContact = formInfo.defaultContact
             defaultBatch = formInfo.defaultBatch
         except:
@@ -704,7 +705,7 @@ def callList(request,daynum,batchid=0):
 def newSpreadsheet(request):
     batches = Batch.objects.all()
     fest = FestivalInfo.objects.last()
-    forminfos = FormInfo.objects.all()
+    forminfos = FormInfo.objects.filter(festival=fest)
     fielddict = {}
     for fi in forminfos:
         fields = json.loads(fi.fields)
@@ -880,7 +881,8 @@ def proposal(request,id):
     if len(shortdesc) > 140:
             infodict['description_short'] = shortdesc[:140] + "..."
     url = infodict['website'] if 'website' in infodict.keys() else ''
-    forminfo = FormInfo.objects.get(showType=infodict['type'])
+    fest = FestivalInfo.objects.last()
+    forminfo = FormInfo.objects.get(showType=infodict['type'],festival=fest)
     formfields = json.loads(forminfo.fields)
     fieldlist = []
     for f in formfields:
